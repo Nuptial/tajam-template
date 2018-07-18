31d0ea3ace60741f8f0187c27f88c5d04841de54
Alican <alican.camlibel91@gmail.com>
2018-07-18 13:56:58 +0300

    about-video-commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     $('.slider-header').bxSlider();
+    $('.section-about-video').click(function () {
+        var myVideo = document.getElementById("section-about-video-elem");
+
+        if (myVideo.paused) {
+            $('#section-about-video-elem').get(0).play();
+            $('.section-about-video-overlay,.section-about-video-play-icon,.section-about-video-title').hide();
+        } else {
+            $('#section-about-video-elem').get(0).pause();
+            $('.section-about-video-overlay,.section-about-video-play-icon,.section-about-video-title').show();
+        }
+
+    });
 });
