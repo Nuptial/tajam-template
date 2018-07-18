2dba4a6433e8aa85925106d787520e47ee2025f5
Alican <alican.camlibel91@gmail.com>
2018-07-18 18:47:21 +0300

    section team and works commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,41 @@ var videoEndCallback = function (e) {
     $('.section-about-video-overlay,.section-about-video-play-icon,.section-about-video-title').show();
 }
 
+var navListItemsActions = function () {
+
+    $('.right-nav-list-item:eq(0)').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("body").offset().top
+        }, 1000);
+    });
+
+    $('.right-nav-list-item:eq(1)').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".section-about").offset().top
+        }, 1000);
+    });
+
+    $('.right-nav-list-item:eq(2)').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".section-expertise").offset().top
+        }, 1000);
+    });
+
+    $('.right-nav-list-item:eq(3)').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".section-team").offset().top
+        }, 1000);
+    });
+
+    $('.right-nav-list-item:eq(4)').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".section-works").offset().top
+        }, 1000);
+    });
+
+
+}
+
 $(document).ready(function () {
     $('.slider-header').bxSlider();
 
@@ -18,5 +53,8 @@ $(document).ready(function () {
 
     });
 
+    navListItemsActions();
+
     document.getElementById('section-about-video-elem').addEventListener('ended', videoEndCallback, false);
+
 });
